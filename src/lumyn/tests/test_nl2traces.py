f1b3c99d9f981596fcaadc70fa70ede4ce7b61e5
Noah Zheutlin <Noah.Zheutlin@ibm.com>
2025-04-15 09:33:54 -0400

    fix nl2traces test

--- src/lumyn/tests/test_nl2traces.py
+++ src/lumyn/tests/test_nl2traces.py
@@ -15,21 +15,24 @@
 
 import logging
 
+from dotenv import load_dotenv
+
 from lumyn.llm_backends.init_backend import get_llm_backend_for_tools
-from lumyn.tools.grafana.nl2traces import NL2TracesCustomTool
+from lumyn.tools.observability_stack.nl2traces import NL2TracesCustomTool
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
+load_dotenv()
 
-# def test_nl2traces():
-#     nl2traces_tool = NL2TracesCustomTool(llm_backend=get_llm_backend_for_tools())
-#     nl_query = "get traces from the frontend service in the last hour."
-#     result = nl2traces_tool._run(nl_query)
-#     logger.info(f"Result from NL2Traces tool: \n{result}")
-#     print(result)
+def test_nl2traces():
+    nl2traces_tool = NL2TracesCustomTool(llm_backend=get_llm_backend_for_tools())
+    nl_query = "get traces from the frontend service in the last hour."
+    result = nl2traces_tool._run(nl_query)
+    logger.info(f"Result from NL2Traces tool: \n{result}")
+    print(result)
 
-# if __name__ == "__main__":
-#     test_nl2traces()
+if __name__ == "__main__":
+    test_nl2traces()
 
 

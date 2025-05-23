39f57c103bcd802b9f838c5a82d8de5cb976e273
Rohan R. Arora <rohan.arora@ibm.com>
2025-05-23 14:01:01 -0400

    wip: finops related crew and configurations
    Signed-off-by: Rohan R. Arora <rohan.arora@ibm.com>

---
+++ src/lumyn/crew_finops.py
@@ -0,0 +1,116 @@
+# Copyright contributors to the ITBench project. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+
+# from chromadb.utils.embedding_functions.openai_embedding_function import OpenAIEmbeddingFunction
+from crewai import LLM, Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from dotenv import load_dotenv
+
+from lumyn.llm_backends.init_backend import (get_llm_backend_for_agents,
+                                                    get_llm_backend_for_tools)
+from lumyn.tools.observability_stack.clickhouse_nl2sql import NL2SQLClickHouseCustomTool
+
+load_dotenv()
+
+
+@CrewBase
+class LumynFinOpsCrew():
+
+    def __init__(self, callback_agent=None, callback_task=None):
+        self.callback_task = None
+        self.callback_agent = None
+
+        if os.getenv("AGENT_TASK_DIRECTORY"):
+            print("Using custom configuration...")
+            self.agents_config = os.path.join(
+                os.getenv("AGENT_TASK_DIRECTORY"), "finops_agents.yaml")
+            self.tasks_config = os.path.join(os.getenv("AGENT_TASK_DIRECTORY"),
+                                             "finops_tasks.yaml")
+        else:
+            # TODO: Switch to logging
+            print("Using default configuration...")
+            self.agents_config = "config/finops_agents.yaml"
+            self.tasks_config = "config/finops_tasks.yaml"
+
+        try:
+            if callback_agent is not None and callback_task is not None:
+                self.callback_task = callback_task
+                self.callback_agent = callback_agent
+        except KeyError as e:
+            print("No handlers (or one of the handlers) spotted at this time:",
+                  e)
+
+    @agent
+    def finops_data_query_agent(self) -> Agent:
+        return Agent(config=self.agents_config["finops_data_query_agent"],
+                     llm=get_llm_backend_for_agents(),
+                     tools=[
+                        NL2SQLClickHouseCustomTool(llm_backend=get_llm_backend_for_tools())
+                     ],
+                     allow_delegation=False,
+                     max_iter=20,
+                     step_callback=self.callback_agent,
+                     verbose=True,
+                     respect_context_window=True,
+                     human_input=False)
+
+    @task
+    def finops_data_query_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["finops_data_query_task"],
+            verbose=True,
+            tools=[
+                NL2SQLClickHouseCustomTool(llm_backend=get_llm_backend_for_tools()),
+            ],
+            human_input=False)
+
+    @crew
+    def crew(self) -> Crew:
+        if os.getenv("MODEL_EMBEDDING"):
+            if os.getenv("PROVIDER_AGENTS") == "azure":
+                memory = True
+                embedder = {
+                    "provider": "azure",
+                    "config": {
+                        "api_type": "azure",
+                        "api_key": os.getenv("API_KEY_AGENTS"),
+                        "api_base": os.getenv("URL_EMBEDDING"),
+                        "api_version": os.getenv("API_VERSION_EMBEDDING"),
+                        "model_name": os.getenv("MODEL_EMBEDDING")
+                    }
+                }
+            elif os.getenv("PROVIDER_AGENTS") == "watsonx":
+                memory = True
+                embedder = {
+                    "provider": "watson",
+                    "config": {
+                        "model": os.getenv("MODEL_EMBEDDING"),
+                        "api_url": os.getenv("URL_EMBEDDING"),
+                        "api_key": os.getenv("API_KEY_AGENTS"),
+                        "project_id": os.getenv("WX_PROJECT_ID"),
+                    }
+                }
+        else:
+            memory = False
+            embedder = None
+
+        return Crew(agents=self.agents,
+                    tasks=self.tasks,
+                    process=Process.sequential,
+                    memory=memory,
+                    embedder=embedder,
+                    verbose=True)
---
+++ src/lumyn/main_finops.py
@@ -0,0 +1,79 @@
+# Copyright contributors to the ITBench project. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+#!/usr/bin/env python
+import json
+import sys
+import subprocess
+
+from lumyn.crew_finops import LumynFinOpsCrew
+
+# This main file is intended to be a way for your to run your
+# crew locally, so refrain from adding necessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    try:
+        subprocess.run('crewai reset-memories -a', shell=True, capture_output=False, text=True)
+    except:
+        print("no memories to clear")
+
+    LumynFinOpsCrew().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "FinOps-related diagnosis for an IT environment."
+    }
+    try:
+        LumynCrew().crew().train(n_iterations=int(sys.argv[1]),
+                                 filename=sys.argv[2],
+                                 inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        LumynCrew().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "Problem diagnosis and remediation for an IT environment."
+    }
+    try:
+        LumynCrew().crew().test(n_iterations=int(sys.argv[1]),
+                                openai_model_name=sys.argv[2],
+                                inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
